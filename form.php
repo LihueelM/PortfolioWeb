c1aeccee771526139e1680c15aa0c0e343d7a955
lihueelmansilla <lihueel.m@hotmail.com>
2022-06-06 18:26:39 -0300

    final

--- form.php
+++ form.php
@@ -1,26 +1,18 @@
-    <?php
-    // Conecto mi form con el server a traves de los name
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $mensaje = $_POST['textarea'];
-    ​
-    // Como funciona con el server
-    $header = 'From: ' . $email . " \r\n";
-    $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-    $header .= "Mime-Version: 1.0 \r\n";
-    $header .= "Content-Type: text/plain";
-    ​
-    // Como me a llegar el cuerpo del mail a mi, o sea lo que la gente escribió en el form
-    $mensaje = "Este mensaje fue enviado por " . $name . ",\r\n";
-    $mensaje .= "Su e-mail es: " . $email . " \r\n";
-    $mensaje .= "Su mensaje es" .$textarea . " \r\n";
-    $mensaje .= "Enviado el " . date('d/m/Y', time());
-    ​
-    $para = 'lihueel.m@hotmail.com'; // El mail a donde van a llegar los mensajes
-    $asunto = 'Mensaje de mi sitio web';
-    ​
-    mail($para, $asunto, utf8_decode($mensaje), $header);
-    ​
-    header('Location: index.html');
-    ​
-    ?>
+<?php
+
+$nombre = $_POST["name"];
+$email = $_POST["email"];
+$mensaje = $_POST["textarea"];
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje = "Su e-mail es: " . $email . " \r\n";
+$mensaje = "El mensaje es: " . $_POST["mensaje"] . " \r\n";
+$mensaje = "Enviado el " . date('d/m/y', time());
+
+$para = "lihueel.m@hotmail.com";
+$asunto = "Este mail fue enviado desde Portfolio ";
+
+mail($para, $asunto, utf_decode($mensaje),$header);
+header("Location:index.html");
+
+?>
